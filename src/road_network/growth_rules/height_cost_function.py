c0e79e0b892f45a44f3de81f4f6f41bfc597c7f6
Benjamin Goh <benjaminjzgoh@gmail.com>
2023-08-14 17:49:37 +1200

    Added comments for what to do next

--- src/road_network/growth_rules/height_cost_function.py
+++ src/road_network/growth_rules/height_cost_function.py
@@ -8,15 +8,27 @@ def get_height_map():
     height_map = parse_image(path + "/input/images/height_map.png")
     return height_map
 
-
+# Replace the major road generation using A* search
 def height_cost_function(map):
     gray = rgb2gray(map)
     height, width = gray.shape
     for x in range(width):
         for y in range(height):
             # Replace with function for pixel value
-            print(gray[y][x])
+            pixel = gray[y][x]
+            if x == width - 1:
+                continue
+            neighbour = gray[y][x+1]
+            # Given 2 neighbouring pixels
+            # Height Cost = abs(pixel1 - pixel2)
+            # Get absolute distance between pixel1 and pixel2 as a multiplier to the cost
+            # If abs(pixel1 - pixel2) > slope_threshold, then cost = inf
+            # Assume water depth from water maps?
+            difference = pixel
 
+    # Generate a path between pixel1 and pixel100
+    # Perform A* search between pixel1 and pixel100
+    # Cost = cost function
 
 
 if __name__ == "__main__":
