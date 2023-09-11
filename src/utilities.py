e534d8df07d6f3dea39f9d98f9322d159a796cb7
Nick <thenickys123@gmail.com>
2023-09-11 20:26:45 +1200

    Rename get_distance_between_2_points()

--- src/utilities.py
+++ src/utilities.py
@@ -154,5 +154,5 @@ def parse_json(filepath):
     return data
 
 
-def calculate_distance_between_2_points(point1, point2):
+def get_distance_between_2_points(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
